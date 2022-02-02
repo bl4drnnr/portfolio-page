446a054ff713f89618e840c0008c9c74a38e88d1
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-02 19:37:30 +0100

    plain page styles

--- spa-portfolio/src/Components/PlainPage.js
+++ spa-portfolio/src/Components/PlainPage.js
@@ -1,9 +1,35 @@
 import React from "react";
 
+const styles = {
+  aboutMe: {
+    width: '100%',
+    height: '50vh',
+    backgroundColor: 'green'
+  },
+  myProject: {
+    width: '100%',
+    height: '50vh',
+    backgroundColor: 'red'
+  },
+  contact: {
+    width: '100%',
+    height: '50vh',
+    backgroundColor: 'blue'
+  }
+}
+
 export default function PlainPage() {
   return (
     <div className="plainPage">
+      <div style={styles.aboutMe}>
 
+      </div>
+      <div style={styles.myProject}>
+
+      </div>
+      <div style={styles.contact}>
+
+      </div>
     </div>
   )
 }
