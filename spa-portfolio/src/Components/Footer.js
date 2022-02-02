c489aa6b400a2d20ffd06b310bb973493d279183
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-02 19:06:15 +0100

    footer

--- spa-portfolio/src/Components/Footer.js
+++ spa-portfolio/src/Components/Footer.js
@@ -3,7 +3,16 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-
+      <div className="inner-footer">
+        <div className="links">
+          <p>About me</p>
+          <p>My projects</p>
+          <p>Contact</p>
+        </div>
+        <div className="copyright">
+          <p>All rights reserved</p>
+        </div>
+      </div>
     </div>
   )
 }
--- spa-portfolio/src/Components/Header.js
+++ spa-portfolio/src/Components/Header.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function Header() {
   return (
     <div className="header">
-
+      <div className="inner-header">
+        <p>About me</p>
+        <p>My projects</p>
+        <p>Contact</p>
+      </div>
     </div>
   )
 }
