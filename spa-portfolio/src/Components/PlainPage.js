b2727ac809cb0a0e14e732d589619f703d9f8548
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-03 19:23:22 +0100

    content component

--- spa-portfolio/src/Components/PlainPage.js
+++ spa-portfolio/src/Components/PlainPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Content from "./Content";
 
 const styles = {
   aboutMe: {
@@ -20,7 +21,7 @@ const styles = {
     width: '50%',
     height: '100%',
     backgroundColor: '#131313',
-    borderTopLeftRadius: '65px'
+    borderTopLeftRadius: '65px',
   },
   imgRight: {
     width: '50%',
@@ -39,14 +40,22 @@ const styles = {
   },
   text: {
     padding: '35px'
+  },
+  content: {
+    paddingTop: '35px'
   }
 }
 
 export default function PlainPage() {
+  const contactItems = [{}, {}, {}]
   return (
     <div className="plainPage">
       <div style={styles.aboutMe}>
-        <div style={styles.img} />
+        <div style={styles.img}>
+          <div style={styles.content}>
+            <Content />
+          </div>
+        </div>
         <div style={styles.textWrap}>
           <div style={styles.text}>
             <code><span className="begin">Hello</span>, my name is ..., nickname is bl4drnnr. I'm web developer from Belarus, who lives in Poland now. The story of this SPA creation is fun. When I have created something you can find below (in short, this is my blog), I have recognized that this works on 5 docker containers and on AWS for free trial I have only 1 instance with 1 GM RAM (lol) and it was impossible to deploy it that way, as a result you see this.</code>
@@ -68,7 +77,7 @@ export default function PlainPage() {
         <div style={styles.imgBottom} />
         <div style={styles.textWrap}>
           <div style={styles.text}>
-            <code>And here is the list of places where you can find and text me, feel free to do it, I'll be glad to receive any feedback.</code>
+            <code><span className="begin">And</span> here is the list of places where you can find and text me, feel free to do it, I'll be glad to receive any feedback.</code>
           </div>
         </div>
       </div>
