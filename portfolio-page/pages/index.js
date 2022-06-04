0347cdd66fb4d447079d3292943eb48cad9992a6
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-04 10:59:21 +0200

    scss

--- portfolio-page/pages/index.js
+++ portfolio-page/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -20,6 +20,19 @@ export default function Home() {
         </div>
         <div className={styles.content}></div>
       </main>
+
+      {/*<footer className={styles.footer}>*/}
+      {/*  <a*/}
+      {/*    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Powered by{' '}*/}
+      {/*    <span className={styles.logo}>*/}
+      {/*      <Image src="/nextjslogo.png" alt="Vercel Logo" width={80} height={48} />*/}
+      {/*    </span>*/}
+      {/*  </a>*/}
+      {/*</footer>*/}
 
     </div>
   )
