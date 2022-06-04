fab898a5fcb3ea96a43baf44ce5db593a2e43772
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-04 12:27:47 +0200

    404

--- portfolio-page/pages/404.js
+++ portfolio-page/pages/404.js
@@ -1,12 +1,19 @@
+import Head from "next/head"
+import Link from "next/link"
 import styles from "../styles/404.module.scss";
 
 export default function ErrorPage() {
   return (
     <div>
+
+      <Head>
+        <title>Mikhail Bahdashych | 404</title>
+      </Head>
+
       <main className={styles.main}>
         <div className={styles.errors}>
           <h1>Nah... C'mon, this is portfolio, there are no routers here :(</h1>
-          <h2>Go back home</h2>
+          <h2><Link href={'/'}><a>Go back home</a></Link></h2>
         </div>
       </main>
     </div>
