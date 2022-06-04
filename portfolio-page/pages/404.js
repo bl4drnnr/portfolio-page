3d021cbff12b25873a820c74990cbfd4f0591252
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-04 12:17:52 +0200

    404

--- portfolio-page/pages/404.js
+++ portfolio-page/pages/404.js
@@ -1,5 +1,14 @@
+import styles from "../styles/404.module.scss";
+
 export default function ErrorPage() {
   return (
-    <h1>Error 404</h1>
+    <div>
+      <main className={styles.main}>
+        <div className={styles.errors}>
+          <h1>Nah... C'mon, this is portfolio, there are no routers here :(</h1>
+          <h2>Go back home</h2>
+        </div>
+      </main>
+    </div>
   )
 }
--- portfolio-page/pages/index.js
+++ portfolio-page/pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.scss"
 
 export default function Home() {
   return (
@@ -44,7 +44,7 @@ export default function Home() {
 
 
       <footer className={styles.footer}>
-        <p className={styles.poweredBy}>Powered by{' '}</p>
+        <p className={styles.poweredBy}>Powered by{" "}</p>
         <span>
             <Image src="/nextjslogo.png" alt="Next.js Logo" width={80} height={48} />
           </span>
