83a6aa4b112eea17de35983caa0a346014b44a49
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-04 10:47:38 +0200

    fixes

--- portfolio-page/pages/index.js
+++ portfolio-page/pages/index.js
@@ -14,24 +14,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.avatar}>
-          <Image src="/ava.jpeg" alt="Ava" width={300} height={300} />
+        <div className={styles.welcomeContainer}></div>
+        <div className={styles.avatarContainer}>
+          <Image src="/ava.jpeg" alt="Ava" width={350} height={350} className={styles.avatar} />
         </div>
         <div className={styles.content}></div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/nextjslogo.png" alt="Vercel Logo" width={80} height={48} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
