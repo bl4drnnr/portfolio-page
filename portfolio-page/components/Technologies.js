e9cce80238942b112ccd3765a17d48dd80ba4942
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-05 21:03:45 +0200

    hover images

--- portfolio-page/components/Technologies.js
+++ portfolio-page/components/Technologies.js
@@ -15,7 +15,7 @@ export default function Technologies({ technologies }) {
               { Object.keys(technologies[key]).map((key2, idx) => (
                 <li key={idx}>{key2}
                   { technologies[key][key2].map(key3 => (
-                    <span key={key3.name} className={styles.badge}><Image src={key3.name} width={key3.w} height={key3.h} /></span>
+                    <span key={key3.name} className={styles.badge}><Image className={styles.image} src={key3.name} width={key3.w} height={key3.h} /></span>
                   )) }
                 </li>
               )) }
