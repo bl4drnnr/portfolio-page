bd9b4f9c877a756980d40ecff6660d8d2042fab3
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-21 20:34:31 +0200

    remove contact block

--- portfolio-page/components/Contact.js
+++ portfolio-page/components/Contact.js
@@ -10,15 +10,15 @@ export default function Contact() {
         <p>PGP:<Link href={"https://keys.openpgp.org/vks/v1/by-fingerprint/8773E25F4E5B06F60AD9A04151E343BA669AD317"}><a className={"link blue"}> 8773E25F4E5B06F60AD9A04151E343BA669AD317</a></Link></p>
       </div>
 
-      <div className={styles.contactFields}>
-        <div className={styles.inputs}>
-          <Input title={"Your name"} />
-          <Input title={"Message title"} />
-        </div>
-        <p>Message</p>
-        <textarea className={styles.textAreaField}></textarea>
-        <button className={styles.sendMessageButton}>SEND MESSAGE</button>
-      </div>
+      {/*<div className={styles.contactFields}>*/}
+      {/*  <div className={styles.inputs}>*/}
+      {/*    <Input title={"Your name"} />*/}
+      {/*    <Input title={"Message title"} />*/}
+      {/*  </div>*/}
+      {/*  <p>Message</p>*/}
+      {/*  <textarea className={styles.textAreaField}></textarea>*/}
+      {/*  <button className={styles.sendMessageButton}>SEND MESSAGE</button>*/}
+      {/*</div>*/}
     </div>
   )
 }
