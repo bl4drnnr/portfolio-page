bebf3363331275a640be2a59e90a3a5691b76936
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-23 21:02:05 +0200

    remove input component

--- portfolio-page/components/Contact.js
+++ portfolio-page/components/Contact.js
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import Input from "./Input";
 import styles from "../styles/Contact.module.scss"
 
 export default function Contact() {
@@ -9,16 +8,6 @@ export default function Contact() {
         <p>Email:<Link href={"mailto:mikhail.bahdashych@protonmail.com"}><a className={"link blue"}> mikhail.bahdashych@protonmail.com</a></Link></p>
         <p>PGP:<Link href={"https://keys.openpgp.org/vks/v1/by-fingerprint/8773E25F4E5B06F60AD9A04151E343BA669AD317"}><a className={"link blue"}> 8773E25F4E5B06F60AD9A04151E343BA669AD317</a></Link></p>
       </div>
-
-      {/*<div className={styles.contactFields}>*/}
-      {/*  <div className={styles.inputs}>*/}
-      {/*    <Input title={"Your name"} />*/}
-      {/*    <Input title={"Message title"} />*/}
-      {/*  </div>*/}
-      {/*  <p>Message</p>*/}
-      {/*  <textarea className={styles.textAreaField}></textarea>*/}
-      {/*  <button className={styles.sendMessageButton}>SEND MESSAGE</button>*/}
-      {/*</div>*/}
     </div>
   )
 }
