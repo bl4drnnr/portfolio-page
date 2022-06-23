30d74dd4746c433c6fbaca4d22fb0eddc6c81871
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-23 21:52:19 +0200

    default dark

--- portfolio-page/components/ThemeToggleButton.js
+++ portfolio-page/components/ThemeToggleButton.js
@@ -1,11 +1,29 @@
 import styles from "../styles/ThemeToggleButton.module.scss"
 import Image from "next/image";
 
+const setDark = () => {
+  document.documentElement.setAttribute("data-theme", "dark");
+};
+
+const setLight = () => {
+  document.documentElement.setAttribute("data-theme", "light");
+};
+
+const defaultDark = "dark"
+
+const toggleTheme = (e) => {
+  if (e.target.checked)
+    setDark();
+  else
+    setLight();
+};
+
 export default function ThemeToggleButton() {
   return (
     <>
       <div>
-        <input type="checkbox" className={styles.checkbox} id="checkbox" />
+        <input onChange={toggleTheme} defaultChecked={defaultDark}
+          type="checkbox" className={styles.checkbox} id="checkbox"/>
           <label htmlFor="checkbox" className={styles.label}>
             <Image src={"/moon.svg"} width={13} height={13}/>
             <Image src={"/sun.svg"} width={13} height={13}/>
