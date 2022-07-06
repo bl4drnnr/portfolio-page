70ab3932171510e9dcb75becb19c2a0e25d0fc16
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-07-06 17:25:43 +0200

    fixes

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio"
 }
 
 module.exports = nextConfig
